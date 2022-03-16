23656b25eaf83bfabe3c930ea0db5416b0a60f44
unknown <kracok@gmail.com>
2022-03-16 20:29:12 +0100

    added some css and testing commit

--- src/views/ResultsScreen.vue
+++ src/views/ResultsScreen.vue
@@ -11,5 +11,56 @@ import { htmlDecode } from "../util/utils.js";
 </template>
 
 <style scoped>
+.resultScreen {
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 5px 6px rgba(0, 0, 0, 0.23);
+    padding: 100px 0;
+    width: 60%;
+    margin-left: 20%;
+    min-height: 500px;
+}
 
+.toStartButton {
+    width: 150px;
+    height: 50px;
+    font-size: 30px;
+    border-radius: 15px;
+    margin: 20px;
+}
+
+.playerScore {
+
+}
+
+.playerHighscore {
+
+}
+
+.questionsResultsContainer {
+
+}
+
+.questionResults {
+
+}
+
+.questionText {
+
+}
+
+.questionCorrectAnswer {
+
+}
+
+.questionPlayerAnswer {
+
+}
+
+#correctAnswer {
+    color: green;
+}
+
+#correctAnswer::after {
+    content: "+10 Points";
+    color: green;
+}
 </style>
